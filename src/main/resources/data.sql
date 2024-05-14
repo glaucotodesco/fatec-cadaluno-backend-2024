c62c7dc5aa3860f43c10729a06db7d23edf79d39
glauco.todesco <glauco.todesco@gmail.com>
2024-05-14 11:09:49 -0300

    Entidade, Repositorio, Service e AutoWired

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO TBL_STUDENT(NAME, COURSE) VALUES ('Glauco Todesco','Computação');
+INSERT INTO TBL_STUDENT(NAME, COURSE) VALUES ('Maria Silva','Letras');
+INSERT INTO TBL_STUDENT(NAME, COURSE) VALUES ('Fernanda Arruda','Eventos');
